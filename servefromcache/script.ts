7b867e9277643fee5fe3389c01b28b33199149ba
Maciej Derewiański <eco_derma@poczta.onet.pl>
2021-03-17 11:52:43 +0100

    Add promises to functions plus fixes

--- servefromcache/script.ts
+++ servefromcache/script.ts
@@ -1,44 +1,64 @@
-global.fetch = require('node-fetch');
-const axios = require('axios');
+import fetch from 'node-fetch';
+import axios from 'axios';
+
 const fs = require('fs');
 
-function withFetch(query: string) {
+function withFetch<T>(query: string): Promise<T> {
     if(query.length < 3) throw new Error('Not enough digits');
+
     const url = 'https://www.googleapis.com/books/v1/volumes?';
     const path = `./cache/${query}.json`;
-    if(fs.existsSync(path)){
-        fs.readFile(path, (err, data) => {
-            if(err) throw err;
-            console.log('From a file: ')
-            console.log(JSON.parse(data));
-        });
-    } else {
-        const queryToURL = new URLSearchParams({q: query})
-        fetch(url + queryToURL)
-            .then(res => res.json())
-            .then(data => {
-                fs.writeFile(`./cache/${query}.json`, JSON.stringify(data), () => console.log('Written'));
-            })
-    }
+
+    return new Promise((resolve, reject) => {
+        if(fs.existsSync(path)){
+            fs.readFile(path, (err, data) => {
+                if(err) throw err;
+                console.log('From a file: ')
+                resolve(JSON.parse(data));
+            });
+        } else {
+            const queryToURL = new URLSearchParams({q: query})
+            fetch(url + queryToURL)
+                .then(resp => resp.json())
+                .then(data => {
+                    fs.writeFile(`./cache/${query}.json`, JSON.stringify(data), () => console.log(`Saved to ${query}.json`));
+                    resolve(data);
+                })
+                .catch(err => reject(err))
+        }
+    })
+
 }
 
-function withAxios(query: string) {
+function withAxios<T>(query: string): Promise<T> {
     if(query.length < 3) throw new Error('Not enough digits');
+
     const url = 'https://www.googleapis.com/books/v1/volumes?';
     const path = `./cache/${query}.json`;
-    if(fs.existsSync(path)){
-        fs.readFile(path, (err, data) => {
-            if(err) throw err;
-            console.log(`From ${path}: `)
-            console.log(JSON.parse(data));
-        });
-    } else {
-        const queryToURL = new URLSearchParams({q: query})
-        axios.get(url + queryToURL)
-            .then(resp => {
-                fs.writeFile(`./cache/${query}.json`, JSON.stringify(resp.data), () => console.log('Written'));
-            })
-    }
+
+    return new Promise((resolve, reject) => {
+        if(fs.existsSync(path)){
+            fs.readFile(path, (err, data) => {
+                if(err) throw err;
+                console.log('From a file: ')
+                resolve(JSON.parse(data));
+            });
+        } else {
+            const queryToURL = new URLSearchParams({q: query})
+            axios.get(url + queryToURL)
+                .then(resp => {
+                    fs.writeFile(`./cache/${query}.json`, JSON.stringify(resp.data), () => console.log(`Saved to ${query}.json`));
+                    resolve(resp.data);
+                })
+                .catch(err => reject(err))
+        }
+    })
 }
 
-withAxios('clarkson');
+withFetch('clarkson')
+    .then(resp => console.log(resp))
+withAxios('mickiewicz')
+    .then(resp => console.log(resp));
+
+
+    
