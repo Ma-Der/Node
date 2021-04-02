0f5ebc372726c6e2fd859fd3978e0c4461df64a8
Maciej Derewiański <eco_derma@poczta.onet.pl>
2021-04-02 10:24:51 +0200

    None

--- recursivePromise/src/script.ts
+++ recursivePromise/src/script.ts
@@ -1,5 +1,5 @@
 
-const promises = [Promise.resolve('strange'), Promise.resolve(2), Promise.reject(3), Promise.resolve(4), Promise.resolve(5), Promise.resolve(6), new Promise((resolve) => {
+const promises = [Promise.resolve('strange'), Promise.reject(2), Promise.resolve(3), Promise.resolve(4), Promise.reject(5), Promise.resolve(6), new Promise((resolve, reject) => {
     setTimeout(resolve, 1000, 'lucky');
 } )]
 
@@ -8,23 +8,22 @@ const promises = [Promise.resolve('strange'), Promise.resolve(2), Promise.reject
     if(arrayOfPromises.length < 7) throw new Error("Array should have at least 7 promises.");
 
     let promArray: T[] = [];
-    for(const prom of arrayOfPromises) {
-        try {
-            const result = await prom;
-            promArray.push(result);
+        for(const prom of arrayOfPromises) {
+            try {
+                const result = await prom;
+                promArray.push(result);
+            }
+            catch(err) {
+                promArray.push(err);
+                return promArray;
+            }
         }
-        catch(err) {
-            promArray.push(err);
-            return promArray;
-        }
-    }
     return promArray;
 }
 
+
 recursivePromise(promises)
     .then(res => {
-        console.log(res)
+        console.log(res);
     })
     .catch(err => console.log(err))
-
-    
