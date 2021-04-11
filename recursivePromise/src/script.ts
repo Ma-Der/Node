ac1dad8aa2fff4366b9d40f31cf6dfe2fdfca753
Maciej Derewiański <eco_derma@poczta.onet.pl>
2021-04-11 11:33:57 +0200

    Add recursion and other fixes

--- recursivePromise/src/script.ts
+++ recursivePromise/src/script.ts
@@ -1,24 +1,21 @@
 
-const promises = [Promise.resolve('strange'), Promise.reject(2), Promise.resolve(3), Promise.resolve(4), Promise.reject(5), Promise.resolve(6), new Promise((resolve, reject) => {
-    setTimeout(resolve, 1000, 'lucky');
-} )]
+const promises = [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3), Promise.resolve(4), Promise.resolve(5), Promise.resolve(6), Promise.resolve(7)]
 
 
- const recursivePromise = async <T> (arrayOfPromises: Promise<T>[]): Promise<T[]> => {
-    if(arrayOfPromises.length < 7) throw new Error("Array should have at least 7 promises.");
-
-    let promArray: T[] = [];
-        for(const prom of arrayOfPromises) {
-            try {
-                const result = await prom;
-                promArray.push(result);
+ const recursivePromise = async (arrayOfPromises: Promise<unknown>[], index: number = 0, result: unknown[] = []): Promise<unknown[]> => {
+    
+    if(arrayOfPromises.length == result.length) return result;
+            try { 
+                const promRes = await arrayOfPromises[index];
+                result[index] = promRes;
+                index++;
+                recursivePromise(arrayOfPromises, index, result);
             }
             catch(err) {
-                promArray.push(err);
-                return promArray;
+                result[index] = err;
+                return result;
             }
-        }
-    return promArray;
+    return result;
 }
 
 
