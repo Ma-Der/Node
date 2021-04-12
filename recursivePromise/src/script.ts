58f0c5fb8d04cc439874a0a02d8b2992c9725bb9
Maciej Derewiański <eco_derma@poczta.onet.pl>
2021-04-12 10:30:40 +0200

    Fix

--- recursivePromise/src/script.ts
+++ recursivePromise/src/script.ts
@@ -1,21 +1,19 @@
 
-const promises = [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3), Promise.resolve(4), Promise.resolve(5), Promise.resolve(6), Promise.resolve(7)]
+const promises = [Promise.resolve(1), Promise.resolve(2), Promise.reject(3), Promise.resolve(4), Promise.resolve(5), Promise.resolve(6), Promise.resolve(7)]
 
 
  const recursivePromise = async (arrayOfPromises: Promise<unknown>[], index: number = 0, result: unknown[] = []): Promise<unknown[]> => {
-    
-    if(arrayOfPromises.length == result.length) return result;
-            try { 
-                const promRes = await arrayOfPromises[index];
-                result[index] = promRes;
-                index++;
-                recursivePromise(arrayOfPromises, index, result);
-            }
-            catch(err) {
-                result[index] = err;
-                return result;
-            }
-    return result;
+    if(arrayOfPromises.length === result.length) return result;    
+    try { 
+        const promRes = await arrayOfPromises[index];
+        result[index] = promRes;
+        index++;        
+    }
+    catch(err) {
+        result[index] = err;
+        return result;
+    }
+    return recursivePromise(arrayOfPromises, index, result);
 }
 
 
